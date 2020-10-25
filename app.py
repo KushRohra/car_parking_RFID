5f3ef8fcc3c05c8ec0a3f649c39899a2c9e5c2ad
KushRohra <iec2017024@iiita.ac.in>
2020-10-25 12:49:47 +0530

    getting data from the form

--- app.py
+++ app.py
@@ -9,14 +9,28 @@ def index():
 
 @app.route('/admin_login', methods=["GET", "POST"])
 def admin_login():
-	return render_template('admin_login.html')
+	return render_template('./admin/admin_login.html')
 
 @app.route('/admin_register', methods=["GET", "POST"])
 def admin_register():
-	return render_template('admin_register.html')
+	if request.method == 'POST':
+		shop_email = request.form.get('shop_email')
+		shop_name = request.form.get('shop_name')
+		password = request.form.get('password')
+		repeat_password = request.form.get('repeat_password')
+		wheeler_2 = request.form.get('wheeler_2')
+		wheeler_4 = request.form.get('wheeler_4')
+		price_2 = request.form.get('price_2')
+		price_4 = request.form.get('price_4')
+		special_customer = request.form.get('special_customer')
+		discount = request.form.get('discount')
+		if special_customer == '0' :
+    			discount = 0 
+		print(shop_email, shop_name, password, repeat_password, wheeler_2, wheeler_4, price_2, price_4, special_customer, discount, type(special_customer))
+	return render_template('./admin/admin_register.html')
 
 @app.route('/user_login', methods=["GET", "POST"])
 def user_login():
-	return render_template('user_login.html')
+	return render_template('./user/user_login.html')
 
 app.run(debug=True, port=5000)
