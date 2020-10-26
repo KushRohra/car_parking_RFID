14981d0da582e841459ef4db8d10decff6538911
KushRohra <iec2017024@iiita.ac.in>
2020-10-26 10:04:25 +0530

    all tables releated to a parking lot made

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, session, g, redirect, url_for
 import mysql.connector
 
+mydb = mysql.connector.connect(
+		host="localhost", 
+		user="root", 
+		password="", 
+		database="carparking_rfid"
+	)
+mycursor = mydb.cursor()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -29,10 +37,10 @@ def admin_register():
 		wheeler_4 = int(request.form.get('wheeler_4'))
 		price_2 = int(request.form.get('price_2'))
 		price_4 = int(request.form.get('price_4'))
-		special_customer = request.form.get('special_customer')
+		special_customer = int(request.form.get('special_customer'))
 		discount = int(request.form.get('discount'))
-		if special_customer == '0':
-    			discount = 0 
+		if special_customer == 0:
+    			discount = 0
 		price_2_list = []
 		price_4_list = []
 		for i in range(price_2):
@@ -45,6 +53,50 @@ def admin_register():
 				price_4_list.append([hrs, price])
 		bubbleSort(price_2_list)
 		bubbleSort(price_4_list)	
+		query = "INSERT INTO admintable(shop_name, shop_email, shop_password, parking_2, parking_4, pricing_2, pricing_4, special_customers, discount) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+		args = (shop_name, shop_email, password, wheeler_2, wheeler_4, price_2, price_4, special_customer, discount, )
+		mycursor.execute(query, args)
+		mydb.commit()
+		mycursor.execute("SELECT shop_id FROM admintable WHERE shop_id=(SELECT MAX(shop_id) FROM admintable)");
+		temp_id = mycursor.fetchone()
+		id = temp_id[0]
+		
+		tableName = str(id)+"_"+shop_name+"_parking2";
+		create_table = "CREATE TABLE " + tableName + "(id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, lot_no INT(11), parked INT(11))"
+		mycursor.execute(create_table)
+		for i in range(wheeler_2):
+			query = "INSERT INTO " + tableName + "(lot_no, parked) VALUES(%s,%s)"
+			args = ((i+1),0,)
+			mycursor.execute(query, args)
+			mydb.commit()
+
+		tableName = str(id)+"_"+shop_name+"_parking4";
+		create_table = "CREATE TABLE " + tableName + "(id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, lot_no INT(11), parked INT(11))"
+		mycursor.execute(create_table)
+		for i in range(wheeler_4):
+			query = "INSERT INTO " + tableName + "(lot_no, parked) VALUES(%s,%s)"
+			args = ((i+1),0,)
+			mycursor.execute(query, args)
+			mydb.commit()
+		
+		tableName = str(id)+"_"+shop_name+"_special";
+		create_table = "CREATE TABLE " + tableName + "(id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, rfid INT(11))"
+		mycursor.execute(create_table)
+
+		tableName = str(id)+"_"+shop_name+"_pricing";
+		create_table = "CREATE TABLE " + tableName + "(id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, hrs INT(11), cost INT(11), flag INT(11))"
+		mycursor.execute(create_table)
+		for i in range(len(price_2_list)):
+			query = "INSERT INTO " + tableName + "(hrs,cost,flag) VALUES(%s,%s,%s)"
+			args = (price_2_list[i][0], price_2_list[i][1], 0,)
+			mycursor.execute(query, args)
+			mydb.commit()
+		for i in range(len(price_4_list)):
+			query = "INSERT INTO " + tableName + "(hrs,cost,flag) VALUES(%s,%s,%s)"
+			args = (price_4_list[i][0], price_4_list[i][1], 1,)
+			mycursor.execute(query, args)
+			mydb.commit()
+
 		return redirect(url_for('admin_dashboard'))
 	return render_template('./admin/admin_register.html')
 
